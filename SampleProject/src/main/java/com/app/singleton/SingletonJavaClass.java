1553eb748f1189ba09df457530834d99f42b746d
pappuray <pappurana95@gmail.com>
2019-09-02 21:32:14 +0530

    creatted a getaInstance method in singlton class

--- SampleProject/src/main/java/com/app/singleton/SingletonJavaClass.java
+++ SampleProject/src/main/java/com/app/singleton/SingletonJavaClass.java
@@ -1,15 +1,17 @@
 package com.app.singleton;
 
 public class SingletonJavaClass {
-	private static SingletonJavaClass singleton=null;
-	
- private SingletonJavaClass()
- {
-	 
- }
- 
- static
- {
-	 singleton= new SingletonJavaClass();
- }
+	private static SingletonJavaClass singleton = null;
+
+	private SingletonJavaClass() {
+
+	}
+
+	static {
+		singleton = new SingletonJavaClass();
+	}
+
+	public static SingletonJavaClass getInstance() {
+		return singleton;
+	}
 }
